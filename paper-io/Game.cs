6c7dab526e286a3af9ec0b772cca9506a9ea36f6
DemonEyes12 <anemnonov2@gmail.com>
2021-11-24 11:09:28 +0300

    Создал класс игрока

---
+++ paper-io/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paper_io
+{
+    class Game
+    {
+        Player[,] field;
+        Player[] players;
+    }
+}
