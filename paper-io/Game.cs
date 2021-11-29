1580843f3fd78d7abf0ff5f233056fcedc6a79ed
инет <@KPC.RU>
2021-11-29 11:18:36 +0300

    Оьновил массивы

--- paper-io/Game.cs
+++ paper-io/Game.cs
@@ -10,5 +10,11 @@ namespace paper_io
     {
         Player[,] field;
         Player[] players;
+        int n = 5;
+        public Game (int n)
+        {
+            this.field = new Player[10 * n, 10 * n] ;
+            this.players = new Player[n];
+        }
     }
 }
