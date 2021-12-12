1cb1098aa8707057d6f52cfe47f65ca2ceff548f
MotyaMiracle <motyamiracle@gmail.com>
2021-12-12 21:50:41 +0300

    Rework some moments

--- paper-io/Game.cs
+++ paper-io/Game.cs
@@ -10,11 +10,10 @@ namespace paper_io
     public class Game
     {
         Player[,] field;
-        Player[] players;
-        public Game (int n)
+        List<Player> players = new List<Player>();
+        public Game(int players)
         {
-            this.field = new Player[10 * n, 10 * n] ;
-            this.players = new Player[n];
+            this.field = new Player[10 * players, 10 * players] ;
         }
         public void StartGame() // Этот метод должен вызываться из конструктора при создании игры.
         {
@@ -23,9 +22,10 @@ namespace paper_io
             {
                 for (int column = 0; column < field.GetLength(0) - 2; column++)
                 {
-                    /// <summary>
-                    /// Здесь должна быть проверка свободна ли терротория для появления игрока.
-                    /// </summary>
+                    if (CheckPoint(row, column))
+                    {
+                        locations.Add(new Point(row, column));
+                    }
                 }
             }
             if (locations.Count() != 0)
@@ -35,14 +35,30 @@ namespace paper_io
                 CreatePlayer(location);
             }
         }
+        private bool CheckPoint(int row, int column)
+        {
+            for (int r = row; r < row + 3; r++)
+            {
+                for (int c = column; c < column + 3; c++)
+                {
+                    if (field[r, c] != null) return false;
+                }
+            }
+            return true;
+        }
         /// <summary>
         /// Метод для создания игрока на поле размером 3х3
         /// </summary>
         public void CreatePlayer(Point location)
         {
-            /// <summary>
-            /// Как то нужно добавлять в наш массив
-            /// </summary>
-        }
+            Player player = new Player(new Point(location.X + 1, location.Y + 1));
+            players.Add(player);
+            for (int i = (int)location.X; i < location.X + 3; i++)
+            {
+                for (int j = (int)location.Y; j < (int)location.Y + 3; j++)
+                {
+                    field[i, j] = player;
+                }
+            }
     }
 }
