fcee24f55b9d5c6fd406c8ef815b010ba9b4c824
nikitavagonov <nikita.vaganov@yandex.ru>
2021-12-20 23:25:35 +0300

    Реализованы классы Game и Player.

--- paper-io/Game.cs
+++ paper-io/Game.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
 
 namespace paper_io
 {
-    class Game
+    public class Game
     {
-        Player[,] field;
-        Player[] players;
-        int n = 5;
-        public Game (int n)
+        private Player[,] field;
+        private List<Player> players;
+        private static List<Color> colorsList = new List<Color>
         {
-            this.field = new Player[10 * n, 10 * n] ;
-            this.players = new Player[n];
+            (Color)ColorConverter.ConvertFromString("#FFFF0000"),
+            (Color)ColorConverter.ConvertFromString("#FF0000FF"),
+            (Color)ColorConverter.ConvertFromString("#FF008000"),
+            (Color)ColorConverter.ConvertFromString("#FFFFFF00"),
+            (Color)ColorConverter.ConvertFromString("#FFFFA500"),
+            (Color)ColorConverter.ConvertFromString("#FFADD8E6"),
+            (Color)ColorConverter.ConvertFromString("#FFA52A2A"),
+            (Color)ColorConverter.ConvertFromString("#FF000000"),
+            (Color)ColorConverter.ConvertFromString("#FFEE82EE"),
+            (Color)ColorConverter.ConvertFromString("#FFFFC0CB"),
+        };
+
+        public Game(int countPlayers)
+        {
+            field = new Player[countPlayers * 10, countPlayers * 10];
+            for (int i = 0; i < countPlayers; i++)
+            {
+                this.players.Add(new Player(new Point(), colorsList[i]));
+            }
+        }
+        public void PlacePlayer(Player player, Point point)
+        {
+            int x = (int)point.X;
+
+            int y = (int)point.Y;
+            for (int i = x; i < x + 3; i++)
+            {
+                for (int j = y; j < y + 3; j++)
+                {
+                    field[i, j] = player;
+                }
+            }
+            player.Location = new Point(x + 1, y + 1);
         }
     }
 }
--- paper-io/Player.cs
+++ paper-io/Player.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
 
 namespace paper_io
 {
-    class Player
+    public class Player
     {
+        private Color color;
+        public Point Location;
+        public Player(Point location, Color playerColor)
+        {
+            this.color = playerColor;
+            this.Location = location;
+        }
     }
 }
